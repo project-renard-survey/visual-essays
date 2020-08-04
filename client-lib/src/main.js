b382faac0337cd7d0ba473b492ef3f1fb224db75
Ron Snyder <Ronald.Snyder@ithaka.org>
2020-08-03 20:40:44 -0400

    Fixed bad import

--- client-lib/src/main.js
+++ client-lib/src/main.js
@@ -37,7 +37,7 @@ import _ from 'lodash'
 
 import MobileDetect from 'mobile-detect'
 
-const VERSION = '0.7.24'
+const VERSION = '0.7.25'
 
 console.log(window.location.hostname)
 const componentsBaseURL = window.location.hostname === 'localhost' ? '' : 'https://jstor-labs.github.io/visual-essays'
@@ -45,7 +45,6 @@ const componentsBaseURL = window.location.hostname === 'localhost' ? '' : 'https
 const customScripts = [
   'https://cdnjs.cloudflare.com/ajax/libs/openseadragon/2.4.2/openseadragon.min.js',
   'https://storiiies.cogapp.com/assets/demos/viewer/js/shortcode.js',  // for Storiiies
-  'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
   'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js',
   'https://cdn.rawgit.com/mejackreed/Leaflet-IIIF/v2.0.1/leaflet-iiif.js',
   'https://cdn.rawgit.com/digidem/leaflet-side-by-side/6ea4567fe8d1b3e6ea3b0e9ca05d311f5be125a3/leaflet-side-by-side.js'
